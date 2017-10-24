3f7f7aaa7e46f991036d8a6cfd382cc05e4dd93a
Avi Goldman <avrahamymgoldman@gmail.com>
2017-10-24 08:54:02 -0700

    moved  redirect template to use react-router redirect

--- src/templates/redirect.js
+++ src/templates/redirect.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Redirect from '../components/Redirect'
+import { Redirect } from 'react-router'
 
 export default ({ pathContext }) => {
   return <Redirect to={pathContext.to} />
